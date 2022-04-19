8b2c33b973e8be4e53f39b3faa2e3d9675b860c6
surajgupta64 <72219047+surajgupta64@users.noreply.github.com>
2022-04-19 14:34:44 +0530

    Create shape.js

---
+++ shape.js
@@ -0,0 +1,20 @@
+class Shape{
+    constructor(name){
+       this.name;
+
+    }
+
+    drawShape(){
+        return`${this.name} is drawn`;
+    }
+
+    calculateArea(length,width){
+        return 2*(length*width);
+        
+    }
+    color(color){
+        return `${color} is Applied on ${this.name}`;
+    }
+    
+}
+module.exports = Shape;
